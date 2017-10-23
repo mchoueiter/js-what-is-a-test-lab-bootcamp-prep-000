369cf0c97eb0533d858567036874d0a264a77a0c
Mary Choueiter <mchoueiter@gmail.com>
2017-10-23 15:49:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 // declare the variable
 var word
- 
+
 // assign a value to the variable
 word = 'bird'
- 
-console.log(word) // 'bird'
- 
+
+//console.log(word) // 'bird'
+
 // assign another value to the variable
 word = 'dog'
- 
-console.log(word) // 'dog'
+
+//console.log(word) // 'dog'
+console.log(window.word)
+
+var a = 5, b = 2, c = 3, d = 'hello', e = 'goodbye';
+console.log(a, b, c, d, e)
